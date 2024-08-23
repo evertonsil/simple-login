303f825660556f5f5837001ca5822d0571e02ec0
evertonsil <everton.marques@uni9.edu.br>
2024-08-23 11:34:46 -0300

    removed styles.css not used

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/sweetalert2.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <header class="d-flex justify-content-center align-items-center text-center bg-success text-light">
     <img src="assets/images/logo.jpg" width="100">
